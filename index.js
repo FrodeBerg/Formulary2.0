1e170dcbee7194834c93d8ec8944f63eb3a306d0
FrodeBerg <bergstedt.frode@gmail.com>
2023-01-20 14:38:38 +0100

    first js formula test

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("https://frodeberg.github.io/Formulary2.0/data.json")
-        .then(Response => Response.json())
-        .then(data => {
-            console.log(data);
-        });
+    data();
 })
 
-function test() {
+function get_data() {
     fetch("https://frodeberg.github.io/Formulary2.0/data.json")
     .then(Response => Response.json())
     .then(data => {
-        console.log(data);
-    });  
+        // Parse data and loop through every category
+        object = JSON.parse(data)
+        object.forEach(category => {
+            append_category(category)
+        })
+    });
+}
+
+function append_category(category) {
+
+        // Categories tab   
+        nav = document.getElementById("categories")
+
+        // Title for each category 
+        h3 = document.createElement("h3");
+        h3.innerHTML = category.title;
+        nav.append(h3)
+
+        // Formula and description for each equation
+        category.equations.forEach(equation => {
+        ul = document.createElement("ul");
+
+        // Formula 
+        li = document.createElement("li");
+        li.innerHTML = Object.keys(equation)
+        ul.append(li)
+
+        // Description
+        li.innerHTML = Object.values(equation)
+        ul.append(li)
+        nav.append(ul)
+        });
 }
