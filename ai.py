d06af222b7f23a9503dd790de57c1183aa071765
FrodeBerg <bergstedt.frode@gmail.com>
2023-01-25 10:39:34 +0100

    working sorting, slow and creates large file

--- ai.py
+++ ai.py
@@ -4,7 +4,8 @@ import os
 actions = {}
 paths = {}
 
-variables = set()
+result_variables = set()
+using_variables = set()
 
 def main():
     with open(f'{os. getcwd()}/data.json') as f:
@@ -26,24 +27,26 @@ def main():
                 if variable not in ["","=", " ", "/", "(", ")", "^", "*", "+", "-", "_", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
                     if "^" in variable:
                         variable = variable[0: variable.find("^")]
-                    variables.add(variable)
                     if isleft:
+                        result_variables.add(variable)
                         left.add(variable)
                     else:
+                        using_variables.add(variable)
                         right.add(variable)
             actions[formula] = [left, right]
 
     print(actions)
     i = 0
-    for result in variables:
+    for result in result_variables:
         print(i)
         path = {}
-        for variable in variables:
+        for variable in using_variables:
             if variable == result:
                 continue
             path[variable] = create_key(result, [variable])
         paths[result] = path
         i += 1
+        
     with open("ai.json", "w") as outfile:
         json.dump(paths, outfile, indent=4)
 
@@ -52,20 +55,21 @@ def create_key(result, previous_variables = [], depth = 1):
     if depth > 3:
         return
     path = {}
-    for variable in variables:
+    for variable in using_variables:
         if variable == result or variable in previous_variables:
             continue     
-        combined_formulas = find_formula(result, previous_variables + [variable])
         
-        if len(combined_formulas) == 0:
+        combined_formulas = find_formula(result, previous_variables + [variable])
+        if not combined_formulas:
             path[variable] = create_key(result, previous_variables + [variable], depth + 1)
         else:
+            print(combined_formulas)
             path[variable] = combined_formulas
 
     return path
 
 # Recursivly go deeper until end condition is met, either result variable or no new variables can be made 
-def find_formula(result, available_variables, formulas = [], combined_formulas = []):
+def find_formula(result, available_variables, formulas = []):
 
     for formula in actions:
         # Checks if formula can be made 
@@ -81,13 +85,12 @@ def find_formula(result, available_variables, formulas = [], combined_formulas =
 
             # Checks if result can be made of new and old variables 
             if all(variable in new_variables + available_variables for variable in result):
-                combined_formulas.append(formulas + [formula])  
-                continue   
+                return formulas + [formula]  
+                 
 
             for variable in new_variables:
-                find_formula(result, available_variables + new_variables, formulas + [formula], combined_formulas)
+                return find_formula(result, available_variables + new_variables, formulas + [formula])
 
-    return combined_formulas
 
 
 
